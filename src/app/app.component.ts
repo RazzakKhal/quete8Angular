cf95189101df85092c2d160d2796e1efe8deab16
Razzakkhal <[khalfallah.razzak@gmail.com](mailto:khalfallah.razzak@gmail.com)>
2023-01-24 14:06:40 +0100

    end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quete8Angular';
+  onomatopoeiaList : string [] = [];
+  onReceiveNewOnomatopia(monono : string){
+this.onomatopoeiaList.push(monono);
+  }
 }
